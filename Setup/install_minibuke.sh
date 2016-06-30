350eea937ca70a4d700cb0c5ce78acebc263945c
阿樊提 <fanjiahe2000@163.com>
2016-06-30 15:38:38 +0800

    Create install_minibuke.sh

---
+++ Setup/install_minibuke.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+sudo apt-get install virtualbox
+
+sudo curl -Lo minikube https://storage.googleapis.com/minikube/releases/v0.4.0/minikube-linux-amd64 && chmod +x minikube && sudo mv minikube /usr/local/bin/
