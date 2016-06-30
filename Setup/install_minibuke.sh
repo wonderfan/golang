d6f094ea7e5ab65cfc16342d2fef3f7d5ff93a0b
阿樊提 <fanjiahe2000@163.com>
2016-06-30 15:39:19 +0800

    Update install_minibuke.sh

--- Setup/install_minibuke.sh
+++ Setup/install_minibuke.sh
@@ -3,3 +3,5 @@
 sudo apt-get install virtualbox
 
 sudo curl -Lo minikube https://storage.googleapis.com/minikube/releases/v0.4.0/minikube-linux-amd64 && chmod +x minikube && sudo mv minikube /usr/local/bin/
+
+minikube start
