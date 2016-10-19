f4682bacd73a2fc255a4846b6906ae6d5502e7ff
阿樊提 <fanjiahe2000@163.com>
2016-10-19 08:41:07 +0800

    Update design.go

--- Kubernetes/design.go
+++ Kubernetes/design.go
@@ -3,3 +3,5 @@
 // The relationship between apiserver and apiserver installer?
 
 // What is the role of apiserver installer?
+
+// The api server installer structure has the group members. This is a great link point.
