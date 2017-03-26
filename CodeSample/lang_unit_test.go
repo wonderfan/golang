3f797516d21fa56b48369da70ae3bc4bf7e081bb
阿樊提 <fanjiahe2000@163.com>
2017-03-26 22:28:26 +0800

    Create lang_unit_test.go

---
+++ CodeSample/lang_unit_test.go
@@ -0,0 +1,9 @@
+package main_test
+
+import "fmt"
+import "testing"
+
+func TestSimpleCase(t *testing.T) {
+	fmt.Println("run the test")
+	println("Use the built-in function to print the content")
+}
