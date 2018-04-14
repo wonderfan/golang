d931c03b75abc02ba4f49c39b73391674c20b314
Afanti <fanjiahe2000@163.com>
2018-04-14 14:11:38 +0800

    Update main.go

--- BlockChain/main.go
+++ BlockChain/main.go
@@ -10,13 +10,10 @@ func main(){
   fmt.Println("Use codes to verify fabric")
   blockFile := "/root/go/src/github.com/hyperledger/fabric/examples/e2e_cli/channel-artifacts/genesis.block"
   data, err := ioutil.ReadFile(blockFile)
-	if err != nil {
+  if err != nil {
 		fmt.Errorf("Could not read block %s", blockFile)
   }
-  block, err := utils.UnmarshalBlock(data)
-	if err != nil {
-		fmt.Errorf("error unmarshaling to block: %s", err)
-  }
-  fmt.Println(utils.GetChainIDFromBlock(block))
+
+  fmt.Println(utils.GetChainIDFromBlockBytes(block))
   fmt.Println(" get what we want")
 }
