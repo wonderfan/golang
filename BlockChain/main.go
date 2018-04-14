da396616d45592082953b8c883bfa18e74409ede
Afanti <fanjiahe2000@163.com>
2018-04-14 13:54:27 +0800

    Create main.go

---
+++ BlockChain/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+  "fmt"
+)
+
+func main(){
+  fmt.Println("Use codes to verify fabric")
+  blockFile := "/root/go/src/github.com/hyperledger/fabric/examples/e2e_cli/channel-artifacts/genesis.block"
+  fmt.Println(" get what we want")
+}
