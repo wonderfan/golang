ddcfaee26fde3372756aa34db07706401e764474
Afanti <fanjiahe2000@163.com>
2018-04-14 14:12:27 +0800

    Update main.go

--- BlockChain/main.go
+++ BlockChain/main.go
@@ -11,9 +11,8 @@ func main(){
   blockFile := "/root/go/src/github.com/hyperledger/fabric/examples/e2e_cli/channel-artifacts/genesis.block"
   data, err := ioutil.ReadFile(blockFile)
   if err != nil {
-		fmt.Errorf("Could not read block %s", blockFile)
+	fmt.Errorf("Could not read block %s", blockFile)
   }
-
-  fmt.Println(utils.GetChainIDFromBlockBytes(block))
+  fmt.Println(utils.GetChainIDFromBlockBytes(data))
   fmt.Println(" get what we want")
 }
