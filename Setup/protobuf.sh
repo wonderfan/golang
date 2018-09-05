38100dc7b84049bd983de5ce045408b942c508b4
Afanti <fanjiahe2000@163.com>
2018-09-05 16:33:03 +0800

    Update protobuf.sh

--- Setup/protobuf.sh
+++ Setup/protobuf.sh
@@ -1,6 +1,8 @@
 
 wget https://github.com/protocolbuffers/protobuf/releases/download/v3.6.1/protoc-3.6.1-linux-x86_64.zip
 
+go get -u github.com/golang/protobuf/protoc-gen-go
+
 protoc  --go_out=plugins=grpc:$GOPATH/src *.proto 
 
 protoc  --go_out=. *.proto
