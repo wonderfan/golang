75bebd98d18550a929c44f6323a5c57777e0678d
Afanti <fanjiahe2000@163.com>
2018-11-26 11:32:13 +0800

    Update kafka.sh

--- BlockChain/kafka.sh
+++ BlockChain/kafka.sh
@@ -9,3 +9,6 @@ docker run -d --name zookeeper1 --add-host zookeeper0.example.com:39.104.145.229
 --add-host zookeeper1.exampe.com:39.104.189.169 --network host -e "ZOO_MY_ID=2" \
 -e "ZOO_SERVERS=server.1=zookeeper0.example.com:2888:3888 server.2=zookeeper1.example.com:2888:3888" \
 -p 2181:2181 -p 2888:2888 -p 3888:3888 hyperledger/fabric-zookeeper
+
+39.104.189.169  zookeeper1.exampe.com
+39.104.145.229  zookeeper0.example.com
